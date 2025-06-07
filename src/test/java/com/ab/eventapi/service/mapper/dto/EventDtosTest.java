b367150e7e6d3c5e3781062efb6e3032799e4361
Andrea Basili <andrea.basili@live.it>
2025-06-07 23:51:22 +0200

    json testing for dtos

--- src/test/java/com/ab/eventapi/service/mapper/dto/EventDtosTest.java
+++ src/test/java/com/ab/eventapi/service/mapper/dto/EventDtosTest.java
@@ -4,10 +4,12 @@ import com.ab.eventapi.model.Event;
 import com.ab.eventapi.service.mapper.dto.event.EventInputDto;
 import com.ab.eventapi.service.mapper.dto.event.EventListDto;
 import com.ab.eventapi.service.mapper.dto.event.EventOutputDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 
@@ -23,16 +25,56 @@ public class EventDtosTest {
     @Autowired
     private JacksonTester<EventListDto> toListJson;
 
-    @Test
-    void testSerializeInput() throws Exception {
-        EventInputDto input = new EventInputDto("title", "description", LocalDateTime.now());
-        
+    private LocalDateTime fixedDateTime;
 
+    @BeforeEach
+    void setUp() {
+        fixedDateTime = LocalDateTime.of(2025, 7, 7, 10, 0);
     }
 
     @Test
-    void testSerializeToOutput() throws Exception {}
+    void testSerializeInput() throws Exception {
+
+        EventInputDto input = new EventInputDto("title", "description", fixedDateTime);
+
+        String eventJson = """
+                {
+                "title": "title",
+                "description": "description",
+                "startsAt": "2025-07-07T10:00:00"
+                }
+                """;
+
+        assertThat(inputJson.write(input)).isEqualToJson(eventJson);
+    }
 
     @Test
-    void testSerializeListElement() throws Exception {}
+    void testSerializeOutput() throws Exception {
+        EventOutputDto output = new EventOutputDto(1L, "title", "description", fixedDateTime);
+
+        String eventJson = """
+                {
+                "id": 1,
+                "title": "title",
+                "description": "description",
+                "startsAt": "2025-07-07T10:00:00"
+                }
+                """;
+
+        assertThat(outputJson.write(output)).isEqualToJson(eventJson);
+    }
+
+    @Test
+    void testSerializeListElement() throws Exception {
+        EventListDto listElement = new EventListDto("title", fixedDateTime);
+
+        String eventJson = """
+                {
+                "title": "title",
+                "startsAt": "2025-07-07T10:00:00"
+                }
+                """;
+
+        assertThat(toListJson.write(listElement)).isEqualToJson(eventJson);
+    }
 }
