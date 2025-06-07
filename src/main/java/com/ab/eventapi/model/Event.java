b78995f65d6819d7fdc8fd13c52f44066c7a893e
Andrea Basili <andrea.basili@live.it>
2025-06-07 17:15:18 +0200

    domain model with properties, constructor, getters and override methods

---
+++ src/main/java/com/ab/eventapi/model/Event.java
@@ -0,0 +1,58 @@
+package com.ab.eventapi.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+// add annotations for JPA mirroring
+public class Event {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime startsAt;
+
+    public Event() {
+    }
+
+    public Event(Long id, String title, String description, LocalDateTime startsAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startsAt = startsAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getStartsAt() {
+        return startsAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(id, event.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        // add dateTime to String
+        return "Event: " + title;
+    }
+
+    // private method to convert the dateTime to String
+}
