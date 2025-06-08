780200313f9dadce0aea6c406777db08681eb67b
Andrea Basili <andrea.basili@live.it>
2025-06-08 03:19:57 +0200

    controller still saving to real db

--- src/test/java/com/ab/eventapi/controller/EventControllerTest.java
+++ src/test/java/com/ab/eventapi/controller/EventControllerTest.java
@@ -9,12 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class EventControllerTest {
